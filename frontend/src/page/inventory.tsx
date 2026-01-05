06591b4c5d34af9d42349f8624c4914e74d5706e
Ahmad fatoni <ahfatoni2026@gmail.com>
2026-01-05 18:39:21 +0700

    tambahan inventory

--- frontend/src/page/inventory.tsx
+++ frontend/src/page/inventory.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import SupplierManagement from '@/components/inventasris/SupplierManagement'
+import { Component } from 'react'
 
-const Inventory: React.FC = () => {
-    return (
-        <div className="p-8">
-            <h1 className="text-2xl font-bold text-gray-800 mb-6">Inventory Management</h1>
-            {/* Konten inventory */}
-        </div>
-    );
-};
+export class inventory extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="p-4 rounded-2xl flex justify-between">
+                    <h1 className="text-2xl font-bold text-gray-800 mb-6">Inventory Management</h1>
+                    <div className="flex">
+                        <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">
+                            Add New Supplier
+                        </button>
+                    </div>
+                </div>
+                <div className="grid grid-cols-3 gap-4">
+                    <SupplierManagement />
+                    <SupplierManagement />
+                    <SupplierManagement />
+                    <SupplierManagement />
+                </div>
+            </div>
+        )
+    }
+}
 
-export default Inventory;
+export default inventory
